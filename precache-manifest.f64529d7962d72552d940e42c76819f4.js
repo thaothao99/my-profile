e43c37a7c75986530ae46667198840f06f902f19
Tran Thao <thaothao@SonNguyen.local>
2020-08-01 19:23:00 +0700

    Updates

---
+++ precache-manifest.f64529d7962d72552d940e42c76819f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d652fb291ce87ed881082396e6e1b82d",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "921c3e0609a73b8f548c",
+    "url": "/my-profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43e5f69650c8746437db",
+    "url": "/my-profile/static/js/2.9afbd95a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.9afbd95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921c3e0609a73b8f548c",
+    "url": "/my-profile/static/js/main.c36e55c3.chunk.js"
+  },
+  {
+    "revision": "c1d853bd7e89e86f5c06",
+    "url": "/my-profile/static/js/runtime-main.58046206.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
