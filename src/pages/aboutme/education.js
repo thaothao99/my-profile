0ddb31007826da61ece9241941465f24a9abb7de
Tran Thao <thaothao@SonNguyen.local>
2020-08-25 14:52:06 +0700

    fix edu

--- src/pages/aboutme/education.js
+++ src/pages/aboutme/education.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Row, Col } from 'antd'
+import React from "react";
+import { Row, Col } from "antd";
 
 export default function Education() {
   return (
@@ -7,40 +7,57 @@ export default function Education() {
       <h2>Education</h2>
       <Row className="my-edu">
         <Col span={18} push={6} className="right">
-          <h4><i>Activities</i></h4>
+          <h4>
+            <i>Activities</i>
+          </h4>
           <ul>
             <li>Communicated for Volunteer Spring Campaign 2018.</li>
             <li>Supported for Green Summer Campaign 2018.</li>
-            <li>Organizer of Volunteer Campaign at Mid-Autumn Festival 2017.</li>
-            <li>Member of communication team for BONJOUR UIT - Welcome New Joiner 2018.</li>
-
+            <li>
+              Organizer of Volunteer Campaign at Mid-Autumn Festival 2017.
+            </li>
+            <li>
+              Member of communication team for BONJOUR UIT - Welcome New Joiner
+              2018.
+            </li>
           </ul>
         </Col>
         <Col span={6} pull={18} className="left">
           <div>
-            <h4><i>University of Information Technology VNU-HCM</i></h4>
+            <h4>
+              <i>University of Information Technology VNU-HCM</i>
+            </h4>
             <p>July 2019 – Present</p>
-            <p><i>Major: </i>Information System</p>
-            <p><i>GPA: </i>7.11</p>
+            <p>
+              <i>Major: </i>Information System
+            </p>
+            <p>
+              <i>GPA: </i>7.19
+            </p>
           </div>
         </Col>
       </Row>
       <Row className="my-edu">
         <Col span={18} push={6} className="right">
-          <h4><i>Skills</i></h4>
+          <h4>
+            <i>Skills</i>
+          </h4>
           <ul>
             <li>Speak English fluently and listen well to English.</li>
             <li>Able to research English Documents.</li>
-
           </ul>
         </Col>
         <Col span={6} pull={18} className="left">
           <div>
-            <h4><i>Language - English</i></h4>
-            <p><i>Toeic: </i>590</p>
+            <h4>
+              <i>Language - English</i>
+            </h4>
+            <p>
+              <i>Toeic: </i>690
+            </p>
           </div>
         </Col>
       </Row>
     </div>
-  )
+  );
 }
--- src/pages/aboutme/intro.js
+++ src/pages/aboutme/intro.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Row, Col, Avatar } from 'antd'
-import avt from './avt.jpg'
+import React from "react";
+import { Row, Col, Avatar } from "antd";
+import avt from "./avt.jpg";
 
 export default function Intro() {
   return (
@@ -8,15 +8,27 @@ export default function Intro() {
       <h2>About Me</h2>
       <Row className="row-inf">
         <Col span={18} push={6} className="right">
-          <h4><i>Objective</i></h4>
-          <p>Looking for a challenging role in a reputable organization to utilize my technical, database, management skills for the growth of the organization as well as to enhance my knowledge and working.</p>
-          <h4><i>Qualification</i></h4>
+          <h4>
+            <i>Objective</i>
+          </h4>
+          <p>
+            Looking for a challenging role in a reputable organization to
+            utilize my technical, database, management skills for the growth of
+            the organization as well as to enhance my knowledge and working.
+          </p>
+          <h4>
+            <i>Qualification</i>
+          </h4>
           <ul>
             <li>Experienced with Reactjs.</li>
-            <li>Knowledge about NodeJS especially with framework like NestJS.</li>
+            <li>
+              Knowledge about NodeJS especially with framework like NestJS.
+            </li>
             <li>Proficiency with HTML, CSS, JavaScript.</li>
             <li>Familar with UI frameworks such as Ant Design.</li>
-            <li>Knowledge of modern authorization mechanisms (JSON WebToken).</li>
+            <li>
+              Knowledge of modern authorization mechanisms (JSON WebToken).
+            </li>
             <li>Experience with Babel, Webpack, NPM, Yarn.</li>
             <li>Good understanding of code versioning tools (Git).</li>
             <li>Knowing well about OOP, Data Structures, Algorithms.</li>
@@ -24,11 +36,15 @@ export default function Intro() {
           </ul>
         </Col>
         <Col span={6} pull={18} className="left">
-          <p><Avatar src={avt} size={200} shape="circle"></Avatar></p>
-          <p><i>Nickname: </i>Bowny</p>
-          <p><i>Birhday: </i>16/05/199</p>
+          <p>
+            <Avatar src={avt} size={200} shape="circle"></Avatar>
+          </p>
+          {/* <p><i>Nickname: </i>Bowny</p> */}
+          {/* <p>
+            <i>Birhday: </i>16/05/199
+          </p> */}
         </Col>
       </Row>
     </div>
-  )
+  );
 }
